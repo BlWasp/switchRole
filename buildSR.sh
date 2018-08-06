bbf6d354c954ffa21bc860831c298adc2eb23736
guillaumeDaumas <35003340+guillaumeDaumas@users.noreply.github.com>
2018-08-06 11:24:01 +0200

    Update buildSR.sh

--- buildSR.sh
+++ buildSR.sh
@@ -15,15 +15,16 @@ sudo ld -x --shared -o /lib/x86_64-linux-gnu/security/pam_moduleSR.so pam_module
 
 rm pam_moduleSR.o || exit
 
-gcc -o sr srFolder/sr.c -lpam -lpam_misc -lcap -lcap-ng || exit
-sudo setcap cap_dac_override,cap_setpcap,cap_setfcap+p sr || exit
-gcc -o sr_aux srFolder/sr_aux.c -lcap -lcap-ng || exit
+sudo gcc -o /usr/bin/sr srFolder/sr.c -lpam -lpam_misc -lcap -lcap-ng || exit
+sudo setcap cap_dac_override,cap_setpcap,cap_setfcap+p /usr/bin/sr || exit
+sudo gcc -o /usr/bin/sr_aux srFolder/sr_aux.c -lcap -lcap-ng || exit
 
 sudo cp srFolder/sr /etc/pam.d || exit
 sudo chmod 644 /etc/pam.d/sr || exit
 
 sudo cp capabilityRole.conf /etc/security || exit
 sudo chmod 644 /etc/security/capabilityRole.conf || exit
+rm capabilityRole.conf || exit
 
 sudo chmod 705 $PWD
 
