95e82ceb2a114cdc2723aaa647086b2d62c2fc1b
guillaumeDaumas <35003340+guillaumeDaumas@users.noreply.github.com>
2018-08-06 15:51:15 +0200

    Update buildSR.sh

--- buildSR.sh
+++ buildSR.sh
@@ -9,12 +9,6 @@ sudo apt-get install libcap-dev || exit
 echo "Installation PAM"
 sudo apt-get install libpam0g-dev || exit
 
-gcc -fPIC -fno-stack-protector -c srFolder/pam_moduleSR.c || exit
-
-sudo ld -x --shared -o /lib/x86_64-linux-gnu/security/pam_moduleSR.so pam_moduleSR.o || exit
-
-rm pam_moduleSR.o || exit
-
 sudo gcc -o /usr/bin/sr srFolder/sr.c -lpam -lpam_misc -lcap -lcap-ng || exit
 sudo setcap cap_dac_override,cap_setpcap,cap_setfcap+p /usr/bin/sr || exit
 sudo gcc -o /usr/bin/sr_aux srFolder/sr_aux.c -lcap -lcap-ng || exit
@@ -25,7 +19,5 @@ sudo chmod 644 /etc/pam.d/sr || exit
 sudo cp capabilityRole.conf /etc/security || exit
 sudo chmod 644 /etc/security/capabilityRole.conf || exit
 rm capabilityRole.conf || exit
-
-sudo chmod 705 $PWD
 
 echo "Please restart your computer to make changes effective"
